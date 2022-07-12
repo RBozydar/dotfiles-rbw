f1b248e6869c95945674d7f20002b27853458cec
rbw <wasilewski.robert+github@gmail.com>
2022-07-12 22:19:24 +0200

    add configs

---
+++ home/config/polybar/launch.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+#killall -q polybar
+
+# Wait until the processes have been shut down
+#while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+#polybar bar1 &
+#polybar bar2 &
+#echo "Bars launched..."
+
+
+
+# Terminate any currently running instances
+killall -q polybar
+
+# Pause while killall completes
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload example &
+done
+else
+  polybar --reload top -c ~/.config/polybar/config &
+fi
+
+# Launch bar(s)
+#polybar topDP -q &
+polybar topDVI -q &
+#polybar botDP -q &
+polybar botDVI -q &
+echo "polybars launched..."
+
