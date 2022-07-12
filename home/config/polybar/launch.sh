84c00c19fde59ac85685cff47ade68b2a2207c97
rbw <wasilewski.robert+github@gmail.com>
2022-07-12 23:09:29 +0200

    modify launch

--- home/config/polybar/launch.sh
+++ home/config/polybar/launch.sh
@@ -38,6 +38,7 @@ pkill polybar
 polybar -r main &
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload main &
+    for bar in ['top' ,'bottom']; do
+    MONITOR=$m polybar --reload $bar &
 done
 
