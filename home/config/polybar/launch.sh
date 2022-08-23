a07718aa79eb4cfbd0e9832c787dcad6b785e9f9
Robert Wasilewski <wasilewski.robert@gmail.com>
2022-08-23 20:04:08 +0200

    update polybar

--- home/config/polybar/launch.sh
+++ home/config/polybar/launch.sh
@@ -16,8 +16,8 @@ while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
 OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
 
-for bar in top bottom; do
-    MONITOR=$PRRIMARY polybar --reload $bar &
+for bar in primary_top bottom; do
+    MONITOR=$PRIMARY polybar --reload $bar &
     done
 
 for m in $OTHERS; do
